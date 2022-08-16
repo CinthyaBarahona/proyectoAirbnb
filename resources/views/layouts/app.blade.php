9d12909e766d47a46b1561674fa32ff36f8a539a
Cinthya Barahona <noemybarahona24@gmail.com>
2022-08-15 21:19:54 -0600

    inicio actualizado con inicio de sesion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,14 @@
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
+        <nav class="navbar navbar-expand-md shadow-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo" class="logo"></a>
+            </div>
             <div class="container">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -69,5 +73,14 @@
             @yield('content')
         </main>
     </div>
+    <footer class="footer-inicio">
+        <div class="col-sm-4 d-flex flex-column align-items-right">
+           <a href="#">Nosotros</a>
+           <a href="#">Terminos y Condiciones</a>
+           <a href="#">Contacto</a>
+        </div>
+        <hr>
+        <!---<a href="https://www.flaticon.es/iconos-gratis/cabina" title="cabina iconos">Cabina iconos creados por Freepik - Flaticon</a>--->
+     </footer>
 </body>
 </html>
